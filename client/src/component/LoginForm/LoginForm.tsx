8cce288d3968f88b7a8597f621c8130fec3c00c5
smtnhacker <themikosurara@gmail.com>
2023-03-08 21:00:52 +0800

    refactor (client): fix indention and change id to name in forms

--- client/src/component/LoginForm/LoginForm.tsx
+++ client/src/component/LoginForm/LoginForm.tsx
@@ -4,7 +4,7 @@ const LoginForm = () => {
             <fieldset>
                 <legend>Login</legend>
                 <input className={"loginFormInputFields"} type={"email"} placeholder={"Email"} pattern={"[A-Za-z0-9]+@[A-Za-z0-9]+\.[A-Za-z]+"} required></input>
-                <input id={"password"} className={"loginFormInputFields"} type={"password"} placeholder={"Password"} required></input>
+                <input name={"password"} className={"loginFormInputFields"} type={"password"} placeholder={"Password"} required></input>
                 <input className={"loginFormSubmitButton"} type={"submit"} value={"Login"}></input>
             </fieldset>
         </form>
--- client/src/component/SignupForm/SignupForm.tsx
+++ client/src/component/SignupForm/SignupForm.tsx
@@ -2,103 +2,100 @@ import React, {useState} from 'react';
 import validator from 'validator';
 
 const SignupForm = () => {
-
-
-const [email, setEmail] = useState('');
-const [password, setPassword] = useState('');
-const [confirmpassword, setConfirmPassword] = useState('');
-
-const [errorMessage, setErrorMessage] = useState('');
-const [errorMessage2, setErrorMessage2] = useState('');
-
-
-
-/* Use alerts or not/ can switch to custom error message on handleSubmit (button press)1 */
-
-const handleSubmit = (e: any) => {
-    e.preventDefault();
-    if (email === undefined || email === '' || password === undefined || password === '' || confirmpassword === undefined || confirmpassword ==='') {
-        setErrorMessage2('Fill Up all the Fields')
-    }
-    else if (!validator.isStrongPassword(password,{minLength: 8, minLowercase:1, minUppercase:1, minNumbers:1, minSymbols:1}))
-        setErrorMessage2('Weak Password')
-
-    else if (password !== confirmpassword)
-        setErrorMessage2('Passwords inputted are not the same')
-
-    else 
-        setErrorMessage2('Success')
-    console.log(email,password,confirmpassword)
-}
-
-
-const handleInputChange = (e: any) => {
-    const value = e.target.value
-    const id = e.target.id
-
-    if(id === "email")
-        setEmail(value)
-
-    if(id === "password")
-        setPassword(value)
-
-    if (id === "password")
-        if (validator.isStrongPassword(value,{ minLength: 8, minLowercase:1, minUppercase:1, minNumbers:1, minSymbols:1})) 
-            setErrorMessage('Strong')
-        else
-            setErrorMessage("Weak")
-
-    if(id === "confirmpassword")
-        setConfirmPassword(value);
     
-}
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmpassword, setConfirmPassword] = useState('');
+
+    const [errorMessage, setErrorMessage] = useState('');
+    const [errorMessage2, setErrorMessage2] = useState('');
+
+    /* Use alerts or not/ can switch to custom error message on handleSubmit (button press)1 */
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        if (email === undefined || email === '' || password === undefined || password === '' || confirmpassword === undefined || confirmpassword ==='') {
+            setErrorMessage2('Fill Up all the Fields')
+        }
+        else if (!validator.isStrongPassword(password,{minLength: 8, minLowercase:1, minUppercase:1, minNumbers:1, minSymbols:1}))
+            setErrorMessage2('Weak Password')
+
+        else if (password !== confirmpassword)
+            setErrorMessage2('Passwords inputted are not the same')
+
+        else 
+            setErrorMessage2('Success')
+        console.log(email,password,confirmpassword)
+    }
 
 
-return (
-    <div className="registerpage">
-        {errorMessage2 === '' ? null :
-           <span style={{fontWeight: 'bold',}}> {errorMessage2}</span>}
-        <br />
+    const handleInputChange = (e: any) => {
+        const value = e.target.value
+        const name = e.target.name
 
-       <div className="email">
-           <label className="register_label">Email</label>
-           <input 
-                className="register_input" 
-                type="email" 
-                id="email" 
-                placeholder="Enter your email address"
-                value={email} 
-                onChange = {(e) => handleInputChange(e)}></input>
-       </div>
+        if (name === "email")
+            setEmail(value)
 
-       <div className="password">
-           <label className="register_label">Password</label>
-           <input 
-                className="register_input" 
-                type="password" 
-                id="password"
-                placeholder="Password" 
-                value={password} 
-                onChange = {(e) => handleInputChange(e)}></input> 
-           {errorMessage === '' ? null :
-           <span style={{fontWeight: 'bold', color: 'blue'}}> {errorMessage}</span>}
-       </div>
+        if (name === "password")
+            setPassword(value)
 
-       <div className="confirm-password">
-           <label className="register_label">Confirm Password</label>
-           <input 
-                className="register_input" 
-                type="password" 
-                id="confirmpassword" 
-                placeholder="Confirm Password"
-                value={confirmpassword} 
-                onChange = {(e) => handleInputChange(e)}></input>
-       </div>
+        if (name === "password")
+            if (validator.isStrongPassword(value,{ minLength: 8, minLowercase:1, minUppercase:1, minNumbers:1, minSymbols:1})) 
+                setErrorMessage('Strong')
+            else
+                setErrorMessage("Weak")
 
-       <button onClick={handleSubmit}> Register </button>
+        if (name === "confirmpassword")
+            setConfirmPassword(value);
+        
+    }
 
-    </div>
-)
+
+    return (
+        <div className="registerpage">
+            {errorMessage2 === '' ? null :
+            <span style={{fontWeight: 'bold',}}> {errorMessage2}</span>}
+            <br />
+
+            <div className="email">
+                <label className="register_label">Email</label>
+                <input 
+                    className="register_input" 
+                    type="email" 
+                    name="email" 
+                    placeholder="Enter your email address"
+                    value={email} 
+                    onChange = {(e) => handleInputChange(e)} />
+            </div>
+
+            <div className="password">
+                <label className="register_label">Password</label>
+                <input 
+                    className="register_input" 
+                    type="password" 
+                    name="password"
+                    placeholder="Password" 
+                    value={password} 
+                    onChange = {(e) => handleInputChange(e)} /> 
+                {errorMessage === '' ? null :
+                <span style={{fontWeight: 'bold', color: 'blue'}}> {errorMessage}</span>}
+            </div>
+
+            <div className="confirm-password">
+                <label className="register_label">Confirm Password</label>
+                <input 
+                    className="register_input" 
+                    type="password" 
+                    name="confirmpassword" 
+                    placeholder="Confirm Password"
+                    value={confirmpassword} 
+                    onChange = {(e) => handleInputChange(e)} />
+            </div>
+
+            <button onClick={handleSubmit}> Register </button>
+
+        </div>
+    )
 }
 
 export default SignupForm
