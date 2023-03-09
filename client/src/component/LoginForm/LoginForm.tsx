23881e33314c7094b8871f4cd0504808a81bfd2d
phrankos <rmcagulada@up.edu.ph>
2023-03-09 18:30:57 +0800

    feat (client): add mock login validation

--- client/src/component/LoginForm/LoginForm.tsx
+++ client/src/component/LoginForm/LoginForm.tsx
@@ -1,11 +1,57 @@
+import React, {useState} from 'react';
+
 const LoginForm = () => {
+
+    const [errorMessage, setErrorMessage] = useState("")
+
+    // Mock Data (?)
+    const database = [
+        {
+        email: "admin@budgeteer.com",
+        password: "admin"
+        },
+        {
+        email: "email@website.com",
+        password: "password"
+        }
+    ]
+
+    const errorMessageTexts = {
+        email: "Invalid Email",
+        password: "Invalid Password"
+    }
+
+    const handleSubmit = (event: any) => {
+        event.preventDefault()
+
+        var { email, password } = document.getElementsByName("loginForm")[0] as HTMLFormElement
+        console.log(email.value)
+        console.log(password.value)
+
+        const userCredentials = database.find((user) => user.email === email.value)
+
+        console.log(userCredentials)
+        if (userCredentials) {
+            if (userCredentials.password !== password.value) {
+                setErrorMessage(errorMessageTexts.password)
+            }
+            else {
+                setErrorMessage("Success")
+            }
+        }
+        else {
+            setErrorMessage(errorMessageTexts.email)
+        }
+    }
+
     return (
-        <form>
+        <form name={"loginForm"} onSubmit={handleSubmit}>
             <fieldset>
                 <legend>Login</legend>
-                <input className={"loginFormInputFields"} type={"email"} placeholder={"Email"} pattern={"[A-Za-z0-9]+@[A-Za-z0-9]+\.[A-Za-z]+"} required></input>
+                <input name={"email"} className={"loginFormInputFields"} type={"email"} placeholder={"Email"} pattern={"[A-Za-z0-9]+@[A-Za-z0-9]+.[A-Za-z]+"} required></input>
                 <input name={"password"} className={"loginFormInputFields"} type={"password"} placeholder={"Password"} required></input>
                 <input className={"loginFormSubmitButton"} type={"submit"} value={"Login"}></input>
+                {errorMessage}
             </fieldset>
         </form>
     )	
