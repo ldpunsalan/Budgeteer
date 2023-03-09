133fa9ddbed460d7e04f2f88d157a9c83be9956e
smtnhacker <themikosurara@gmail.com>
2023-03-09 10:56:34 +0800

    feat (client): add basic layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
 import { Outlet, Link } from 'react-router-dom'
 
+import styles from "./App.module.css"
+
 function App() {
   return (
-    <div className="App">
-      <nav>
-        Budgeteer!
+    <div className={styles['app-container']}>
+      <nav className={styles['nav']}>
+        <Link to="/">
+          <h1>Budgeteer</h1>
+        </Link>
         <Link to="/signup">Sign-Up</Link>
         <Link to="/login">Log-In</Link>
       </nav>
