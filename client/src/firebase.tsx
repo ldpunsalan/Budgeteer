689dc58989b889fe749fdc0c90b6ddd68f95f4a0
Pr0gr4m3 <sunnysy012@gmail.com>
2023-03-20 23:37:15 +0800

    firebase implementation

---
+++ client/src/firebase.tsx
@@ -0,0 +1,24 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getAuth } from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDpDR1fm-Pvzo8K9jgGftjAQwiitK1IqjE",
+  authDomain: "budgeteer-bb6a5.firebaseapp.com",
+  databaseURL: "https://budgeteer-bb6a5-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "budgeteer-bb6a5",
+  storageBucket: "budgeteer-bb6a5.appspot.com",
+  messagingSenderId: "897189052924",
+  appId: "1:897189052924:web:33c4a9123050c0c6a27279",
+  measurementId: "G-LQ4447PXDC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app;
