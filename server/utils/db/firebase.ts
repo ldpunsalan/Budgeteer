23b4cb649811b4cba5e6a9f111d8fabb76912db6
smtnhacker <themikosurara@gmail.com>
2023-03-21 12:52:14 +0800

    chore (server): add template for firebase abstraction

---
+++ server/utils/db/firebase.ts
@@ -0,0 +1,14 @@
+// Utility function for setting and getting values from the database
+// ensuring that the values and types are properly accounted for
+
+const db = {
+    set: (key: string, value: any) => {
+        // TO-DO: sets the corresponding key to value
+    },
+    get: (key: string) => {
+        // TO-Do: returns the value at key, or handles if key 
+        // does not exist
+    }
+}
+
+export default db
