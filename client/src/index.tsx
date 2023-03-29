7110fa7e98c844352faf78772b176e998b562ccb
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-03-29 11:21:29 +0800

    chore(client): migrate to Vite

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { 
-  createBrowserRouter,
-  RouterProvider 
-} from 'react-router-dom';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 
 import App from './App';
 import SignupForm from './component/SignupForm/SignupForm';
@@ -53,18 +49,10 @@ const router = createBrowserRouter([
       },
     ]
   }
-])
+]);
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
