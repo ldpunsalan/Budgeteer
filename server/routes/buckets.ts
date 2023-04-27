ce2498b0a6290c09031d6f8820e44440274c710f
smtnhacker <themikosurara@gmail.com>
2023-04-27 13:50:38 +0800

    feat: persist new bucket

--- server/routes/buckets.ts
+++ server/routes/buckets.ts
@@ -9,6 +9,10 @@ router.get('/', async (req: Request, res: Response, next: NextFunction) => {
         const database: any[] | undefined = await db.get('buckets');
         const user = req.session.user;
 
+        if (!user) {
+            return res.status(401).json({ msg: "Client error: Please login" }); 
+        }
+
         if (database === undefined) {
             return res.status(500).json({ msg: "Server error: database connection error" });
         }
@@ -40,5 +44,40 @@ router.get('/all', async (req: Request, res: Response, next: NextFunction) => {
         next(err);
     }
 })
+
+router.post('/new', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const database: any[] | undefined = await db.get('buckets');
+        const user = req.session.user
+
+        if (!user) {
+            return res.status(401).json({ msg: "Client error: Please login" });
+        }
+
+        if (database === undefined) {
+            return res.status(500).json({ msg: "Server error: database connection error" });
+        }
+
+        const body = req.body;
+        
+        const newBucket = {
+            ...body,
+            user
+        }
+
+        database.push(newBucket);
+
+        await db.set('buckets', database)
+
+        res.json({
+            msg: "Successfully added database"
+        });
+
+    } catch (err) {
+        next(err)
+    }
+})
+
+// router.post('/edit', async (req: Request, res: Response))
 
 export default router;
