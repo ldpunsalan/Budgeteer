97fa05955b957abec4860e5d5fb4670985161f9b
smtnhacker <themikosurara@gmail.com>
2023-04-27 10:45:11 +0800

    fix: give only user-relevant buckets

--- server/routes/buckets.ts
+++ server/routes/buckets.ts
@@ -6,6 +6,26 @@ const router: Router = express.Router();
 
 router.get('/', async (req: Request, res: Response, next: NextFunction) => {
     try {
+        const database: any[] | undefined = await db.get('buckets');
+        const user = req.session.user;
+
+        if (database === undefined) {
+            return res.status(500).json({ msg: "Server error: database connection error" });
+        }
+        
+        const userDatabase = database.filter((bucket: any) => bucket.user === user);
+
+        return res.json({
+            data: userDatabase
+        })
+
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.get('/all', async (req: Request, res: Response, next: NextFunction) => {
+    try {
         let database: any[] | undefined = await db.get('buckets');
 
         if (database === undefined) {
