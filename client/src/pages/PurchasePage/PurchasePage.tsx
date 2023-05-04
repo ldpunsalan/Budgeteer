f8f2435597e3ac8d2f7e426a90eb05162aecd388
smtnhacker <themikosurara@gmail.com>
2023-05-04 12:00:20 +0800

    chore: improve UX of purchase page

--- client/src/pages/PurchasePage/PurchasePage.tsx
+++ client/src/pages/PurchasePage/PurchasePage.tsx
@@ -6,6 +6,8 @@ import styles from '../Pages.module.css'
 const PurchasePage = () => {
     const [loading, setLoading] = useState(true)
     const [buckets, setBuckets] = useState([])
+    const [current, setCurrent] = useState('0')
+    const [date, setDate] = useState('0000-00-00')
 
     useEffect(() => {
         const fetchBuckets = async () => {
@@ -21,6 +23,18 @@ const PurchasePage = () => {
         fetchBuckets()
     }, [])
 
+    const handleCurrentChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        e.preventDefault()
+        const cur = e.target.value
+        setCurrent(cur)
+    }
+
+    const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault()
+        const d = e.target.value
+        setDate(d)
+    }
+
     if (loading) {
         return <div>Loading...</div>
     }
@@ -28,37 +42,49 @@ const PurchasePage = () => {
     return (
         <div className={styles['content']}>
             <h2>PURCHASE</h2>
-            <h3>Purchase Name</h3>
-            <input 
-                name="purchaseName"
-                className="PurchasePageInputs"
-                type="text"
-                placeholder='Where did you spend your money?'
-                required/>
-            <h3>Amount</h3>
-            <input 
-                name="purchaseAmount"
-                className="PurchasePageInputs"
-                type="number"
-                min="0"
-                placeholder='How much did you spend?'
-                required/>
-            <h3>Bucket</h3>
-            <select name="bucketName" className="purchasePageInputs">
-                <option value="" disabled selected>Choose the relevant bucket</option>
-                {
-                    buckets.map((bucket : any) => {
-                        return <option value={bucket.id} id={bucket.id}>{bucket.name}</option>
-                    })
-                }
-            </select>
-            <h3>Date</h3>
-            <input
-                name="purchaseDate"
-                className='purchasePageInputs'
-                type="date"
-                required/>
-            <button>Create Purchase</button>
+            <form>
+                <label>
+                    <h3>Purchase Name</h3>
+                    <input 
+                        name="purchaseName"
+                        className="PurchasePageInputs"
+                        type="text"
+                        placeholder='Where did you spend your money?'
+                        required/>
+                </label>
+                <label>
+                    <h3>Amount</h3>
+                    <input 
+                        name="purchaseAmount"
+                        className="PurchasePageInputs"
+                        type="number"
+                        min="0"
+                        placeholder='How much did you spend?'
+                        required/>
+
+                </label>
+                <label>
+                    <h3>Bucket</h3>
+                    <select name="bucketName" className="purchasePageInputs" onChange={handleCurrentChange}>
+                        <option value="" disabled selected>Choose the relevant bucket</option>
+                        {
+                            buckets.map((bucket : any) => {
+                                return <option value={bucket.id} id={bucket.id}>{bucket.name}</option>
+                            })
+                        }
+                    </select>
+                </label>
+                <label>
+                    <h3>Date</h3>
+                    <input
+                        name="purchaseDate"
+                        className='purchasePageInputs'
+                        type="date"
+                        onChange={handleDateChange}
+                        required/>
+                </label>
+                <input type='submit' value='Create Purchase' />
+            </form>
         </div>
     )
 }
